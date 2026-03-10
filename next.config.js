9a2f535f43fac84f8378ee8379bf235cc09cf231
JakobMayerTortechnik <jakob.nmso@gmail.com>
2026-03-10 03:50:02 +0100

    Refactor next.config.js for improved syntax

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -11,22 +9,20 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
       },
     ],
   },
   experimental: {
     allowedDevOrigins: ["https://*.cloudworkstations.dev"],
-  }
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
